ea070093672074321a486b09515bd328e39848a2
RenwilMascarenhas <76239898+RenwilMascarenhas@users.noreply.github.com>
2021-02-01 21:30:58 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,14 +1,50 @@
+const Engine=Matter.Engine;
+const World=Matter.World;
+const Bodies=Matter.Bodies;
+
+var engine, world;
+var fairy1, fairy2, star;
+
 function preload()
 {
    //preload the images here
+   bg = loadImage("starnight.png");
+   fairy= loadAnimation("fairy.png");
+   fairy1=loadAnimation("fairy1.png");
+   fairy2= loadAnimation("fairy2.png");
+   star= loadAnimation("star.png");
+   joySound= loadSound("joyMusic.mp3");
+  
 }
 
 function setup() {
-	createCanvas(800, 750);
-}
+  createCanvas(800, 750); 
+  engine=Engine.create();
+  world=engine.world;
+  var star_options={
+    restitution:1.0 
+   }
+   var fairy_options={
+     isStatic:false 
+   }
+   star.x= starBody.position.x
+   star.y= starBody.position.y
 
+}
 
 function draw() {
   background("black");
+  background(0);
 
+  function keyPressed(){
+  }
+    if(keyCode === LEFT_ARROW){
+       fairy.velocityX = -5;
+    }
+
+    if(keyCode === RIGHT_ARROW){
+       fairy.velocityY = 5;
 }
+}
+  //drawSprites();
+
